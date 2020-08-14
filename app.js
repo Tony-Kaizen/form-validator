036747aaff2a4d7ea59aa56363cb5d99cf92a093
Anthony Heatley <anthonyheatley@Anthonys-MBP.lan>
2020-08-14 14:19:33 -0400

    bug fixes

--- app.js
+++ app.js
@@ -25,35 +25,58 @@ form.addEventListener('submit', (e) => {
   //check name
   if (namePattern.test(name)) {
     form.name.classList.add('success');
+    nameError.innerHTML = '';
   } else {
     form.name.classList.add('error');
     nameError.innerHTML = 'Name must be at least 3 letters!';
   }
 
+  if (!name.length) {
+    nameError.innerHTML = 'You must enter your name!';
+  }
+
   //check username
   if (usernamePattern.test(username)) {
     form.username.classList.add('success');
+    userError.innerHTML = '';
   } else {
     form.username.classList.add('error');
     userError.innerHTML = 'Username must be at least 6 letters!';
   }
 
+  if (!username.length) {
+    userError.innerHTML = 'You must choose a username!';
+  }
+
   //check password
   if (passwordPattern.test(password)) {
     form.password.classList.add('success');
+    passError.innerHTML = '';
   } else {
     form.password.classList.add('error');
     passError.innerHTML = 'Password must be at least 6 characters!';
   }
 
+  if (!password.length) {
+    passError.innerHTML = 'You must choose a password!';
+  }
+
   //confirm password
   if (confirmPass === password) {
     form.confirm.classList.add('success');
+    confirmError.innerHTML = '';
   } else {
     form.password.classList.remove('success');
     form.password.classList.add('error');
     form.confirm.classList.add('error');
     confirmError.innerHTML = 'Passwords do not match!';
+  }
+
+  if (password.length && !confirmPass.length) {
+    confirmError.innerHTML = 'You must confirm your password!';
+  } else if (!confirmPass.length) {
+    confirmError.innerHTML = '';
+    form.confirm.classList.remove('success');
   }
 
   //reset form if success
@@ -62,10 +85,14 @@ form.addEventListener('submit', (e) => {
     && passwordPattern.test(password)
     && confirmPass === password) {
     setTimeout(() => {
-      form.name.classList.remove('success');
-      form.username.classList.remove('success');
-      form.password.classList.remove('success');
-      form.confirm.classList.remove('success');
+      form.name.classList.remove('success', 'error');
+      form.username.classList.remove('success', 'error');
+      form.password.classList.remove('success', 'error');
+      form.confirm.classList.remove('success', 'error');
+      nameError.innerHTML = '';
+      userError.innerHTML = '';
+      passError.innerHTML = '';
+      confirmError.innerHTML = '';
       form.reset();
     }, 3000);
   };
